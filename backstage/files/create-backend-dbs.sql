7f5c6b9de6a4c12a9915f4b176d2876d63806e06
Martina Iglesias Fernandez <martina@weave.works>
2020-09-29 13:16:30 +0200

    Refactor init db script

--- backstage/files/create-backend-dbs.sql
+++ backstage/files/create-backend-dbs.sql
@@ -1,7 +1,11 @@
-create database {{ .Values.appConfig.backend.database.connection.database }};
-grant all privileges on database {{ .Values.appConfig.backend.database.connection.database }} to {{ .Values.global.postgresql.postgresqlUsername }};
+{{ $backendDb := .Values.appConfig.backend.database.connection.database }}
+{{ $lighthouseDb := .Values.lighthouse.database.connection.database }}
+{{ $user := .Values.global.postgresql.postgresqlUsername }}
 
-{{ if not (eq .Values.appConfig.backend.database.connection.database .Values.lighthouse.database.connection.database) }}
-create database {{ .Values.lighthouse.database.connection.database }};
-grant all privileges on database {{ .Values.lighthouse.database.connection.database }} to {{ .Values.global.postgresql.postgresqlUsername }};
+create database {{ $backendDb }};
+grant all privileges on database {{ $backendDb }} to {{ $user }};
+
+{{ if not (eq $backendDb $lighthouseDb) }}
+create database {{ $lighthouseDb }};
+grant all privileges on database {{ $lighthouseDb }} to {{ $user }};
 {{ end }}
